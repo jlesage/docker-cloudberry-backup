49e7d939dccc7039eb673e002bedb79af1a6a5c7
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-09-06 05:44:23 -0400

    Set working directory.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,6 +3,7 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+cd /config
 exec /opt/local/"CloudBerry Backup"/bin/cbbGUI
 
 # vim: set ft=sh :

399d09e833563769615a5b228a86a32fd64fde94
Jocelyn Le Sage <jocelyn@le-sage.com>
2021-11-23 15:09:15 -0500

    For future reference, added environment variable to use to debug QT plugins.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,6 +3,8 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+#export QT_DEBUG_PLUGINS=1
+
 cd /config
 exec /opt/local/"CloudBerry Backup"/bin/cbbGUI
 
